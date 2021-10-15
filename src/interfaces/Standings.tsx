823d757e832d1d2c9c72d40ed11bf6a7b4705de9
fobdev <silvapedro55@gmail.com>
2021-10-15 15:16:50 -0300

    implementada as interfaces de resposta do standings endpoint

--- src/interfaces/Standings.tsx
+++ src/interfaces/Standings.tsx
@@ -10,5 +10,48 @@ export interface StandingsData {
     abbreviation: string;
     seasonDisplay: string;
     seasons: number;
-    standings: [];
+    standings: StandingsResponse[];
+}
+
+export interface StandingsResponse {
+    team: StandingsTeam;
+    note: StandingsNote;
+    stats: StandingsStats[];
+}
+
+export interface StandingsTeam {
+    id: string;
+    uid: string;
+    location: string;
+    name: string;
+    abbreviation: string;
+    displayName: string;
+    shortDisplayName: string;
+    isActive: boolean;
+    logos: StandingsTeamLogos[];
+}
+
+export interface StandingsTeamLogos {
+    href: string;
+    width: number;
+    height: number;
+    alt: string;
+    rel: string[];
+    lastUpdate: string;
+}
+
+export interface StandingsNote {
+    color: string;
+    description: string;
+    rank: number;
+}
+
+export interface StandingsStats {
+    type: string;
+    name: string;
+    displayName: string;
+    shortDisplayName: string;
+    description: string;
+    value: number;
+    displayValue: string;
 }
