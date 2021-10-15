a61e7edce4039892242ae381804e2cc7e6f0ff54
fobdev <silvapedro55@gmail.com>
2021-10-15 13:28:53 -0300

    Home component criado

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+interface Props {}
+export const Home: React.FC<Props> = () => {
+    return (
+        <Box>
+            <Typography>Hello World!</Typography>
+        </Box>
+    );
+};
+export default Home;
