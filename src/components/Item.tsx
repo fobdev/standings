9251c3a6a9eeb19abc2210b5607c7077eeb3e7ed
fobdev <silvapedro55@gmail.com>
2021-10-15 17:13:45 -0300

    adicionado um prop index para gerenciar a animação

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,29 +1,32 @@
 import { Box } from "@mui/system";
-import { Typography, Paper, ButtonBase } from "@mui/material";
+import { Typography, Paper, ButtonBase, Grow } from "@mui/material";
 import * as React from "react";
 import { LeaguesData } from "../interfaces";
 import { mainItemBoxStyling } from "./styles";
 
 interface componentProps {
     onClick: React.MouseEventHandler<HTMLButtonElement>;
+    index: number;
 }
 
 type Props = componentProps & LeaguesData;
-export const Item: React.FC<Props> = ({ id, logos, abbr, name, slug, onClick }) => {
+export const Item: React.FC<Props> = ({ id, logos, abbr, name, slug, onClick, index }) => {
     return (
-        <ButtonBase sx={mainItemBoxStyling} onClick={onClick}>
-            <Paper elevation={5} className="main-box">
-                <img src={logos.light} alt="League Logo" />
-                <Box className="title-box">
-                    <Typography variant="h2" className="title-name">
-                        {name}
-                    </Typography>
-                    <Typography variant="h3" className="title-abbr">
-                        {abbr}
-                    </Typography>
-                </Box>
-            </Paper>
-        </ButtonBase>
+        <Grow in={true} timeout={{ enter: index * 500 }}>
+            <ButtonBase sx={mainItemBoxStyling} onClick={onClick}>
+                <Paper elevation={5} className="main-box">
+                    <img src={logos.light} alt="League Logo" />
+                    <Box className="title-box">
+                        <Typography variant="h2" className="title-name">
+                            {name}
+                        </Typography>
+                        <Typography variant="h3" className="title-abbr">
+                            {abbr}
+                        </Typography>
+                    </Box>
+                </Paper>
+            </ButtonBase>
+        </Grow>
     );
 };
 
