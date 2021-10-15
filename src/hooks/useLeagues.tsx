891fc88654221c6ffbba1653d4f50182104ab23a
fobdev <silvapedro55@gmail.com>
2021-10-15 13:47:01 -0300

    melhor explicação do hook via comments

--- src/hooks/useLeagues.tsx
+++ src/hooks/useLeagues.tsx
@@ -3,15 +3,26 @@ import { Leagues } from "../interfaces";
 import { LeaguesService } from "../services";
 
 export const useLeagues = () => {
+    /**
+     * leagueTasks guardará o conteúdo do ENDPOINT correspondente da API
+     * a call de useLeagues() deve ser executada dentro de um useEffect() hook
+     * por conta de ser uma request assincrona.
+     */
     const [leaguesTasks, setLeagueTasks] = useState<Leagues>();
 
-    // useCallback para mudar apenas quando a depenência é mudada
+    /**
+     * useCallback para mudar apenas quando a depenência é mudada
+     */
     const getAllLeagues = useCallback(async () => {
         const { status, data } = await LeaguesService.getAll();
         if (status !== 200) throw new Error();
         setLeagueTasks(data);
     }, []);
 
+    /**
+     * a função para o fetch e o respectivo objeto serão retornados
+     * assim possibilitando a utilização dos dados pela constante leaguesTasks.
+     */
     return {
         leaguesTasks,
         getAllLeagues,
--- src/hooks/useStandings.tsx
+++ src/hooks/useStandings.tsx
@@ -3,15 +3,26 @@ import { Standings } from "../interfaces";
 import { StandingsService } from "../services/StandingsService";
 
 export const useStanding = () => {
+    /**
+     * standingTasks guardará o conteúdo do ENDPOINT correspondente da API
+     * a call de useStanding() deve ser executada dentro de um useEffect() hook
+     * por conta de ser uma request assincrona.
+     */
     const [standingTasks, setStandingTasks] = useState<Standings>();
 
-    // useCallback para mudar apenas quando a depenência é mudada
+    /**
+     * useCallback para mudar apenas quando a depenência é mudada
+     */
     const getStanding = useCallback(async () => {
         const { status, data } = await StandingsService.getAll();
         if (status !== 200) throw new Error();
         setStandingTasks(data);
     }, []);
 
+    /**
+     * a função para o fetch e o respectivo objeto serão retornados
+     * assim possibilitando a utilização dos dados pela constante standingTasks.
+     */
     return {
         standingTasks,
         getStanding,
