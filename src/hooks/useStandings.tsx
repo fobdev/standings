ac3d1621af1d50ac423a8f1776700a3865f10fe6
fobdev <silvapedro55@gmail.com>
2021-10-15 15:16:20 -0300

    fix: id passado no useCallback

--- src/hooks/useStandings.tsx
+++ src/hooks/useStandings.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 import { Standings } from "../interfaces";
 import { StandingsService } from "../services/StandingsService";
 
-export const useStanding = () => {
+export const useStanding = (id: string) => {
     /**
      * standingTasks guardará o conteúdo do ENDPOINT correspondente da API
      * a call de useStanding() deve ser executada dentro de um useEffect() hook
@@ -13,9 +13,10 @@ export const useStanding = () => {
     /**
      * useCallback para mudar apenas quando a depenência é mudada
      */
-    const getStanding = useCallback(async () => {
-        const { status, data } = await StandingsService.getAll();
+    const getStanding = useCallback(async (id) => {
+        const { status, data } = await StandingsService.getAll(id);
         if (status !== 200) throw new Error();
+
         setStandingTasks(data);
     }, []);
 
