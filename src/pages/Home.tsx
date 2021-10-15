32515de6cb1b06a3bd49f5d757435bfbdc1ca94b
fobdev <silvapedro55@gmail.com>
2021-10-15 16:05:27 -0300

    removido limite de 10 leagues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Dialog, DialogTitle, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { mainHomeBoxStyling } from "./styles/Home";
 import { useEffect, useState } from "react";
-import { Item } from "../components";
+import { Item, StandingListItem } from "../components";
 import useLeagues from "../hooks/useLeagues";
 import useStanding from "../hooks/useStandings";
 
@@ -24,22 +24,20 @@ export const Home: React.FC<Props> = () => {
         <Box sx={mainHomeBoxStyling}>
             <Paper className="main-paper" elevation={5}>
                 {leaguesTasks?.data.map((element, index) => {
-                    if (index < 10)
-                        return (
-                            <Item
+                    return (
+                        <Item
                             key={index}
-                                onClick={() => {
-                                    setStandingKey(element.id);
-                                    setDialogOpen(true);
-                                }}
-                                id={element.id}
-                                abbr={element.abbr}
-                                logos={element.logos}
-                                name={element.name}
-                                slug={element.slug}
-                            />
-                        );
-                    return null;
+                            onClick={() => {
+                                setStandingKey(element.id);
+                                setDialogOpen(true);
+                            }}
+                            id={element.id}
+                            abbr={element.abbr}
+                            logos={element.logos}
+                            name={element.name}
+                            slug={element.slug}
+                        />
+                    );
                 })}
             </Paper>
             <Dialog open={dialogOpen}>
