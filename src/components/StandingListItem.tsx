87ed6f21b1cc6d4d671ca0a63277a9461de0949a
fobdev <silvapedro55@gmail.com>
2021-10-15 19:24:21 -0300

    style modificado para se encaixar corretamente no novo gap

--- src/components/StandingListItem.tsx
+++ src/components/StandingListItem.tsx
@@ -1,30 +1,39 @@
-import { Paper, Typography } from "@mui/material";
+import { Grow, Paper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { StandingsResponse } from "../interfaces";
 import { mainStandingListItemBoxStyling } from "./styles";
 import noimage from "../images/noimage.png";
 
-export const StandingListItem: React.FC<StandingsResponse> = ({ note, stats, team }) => {
+interface componentProps {
+    index: number;
+}
+
+type StandingsProps = componentProps & StandingsResponse;
+
+export const StandingListItem: React.FC<StandingsProps> = ({ note, stats, team, index }) => {
     /**
      * A logica abaixo foi implementada para suprir alguns times que não tem imagem.
      */
     let teamImage = team.logos ? team.logos[0].href : noimage;
     let imageAlt = team.logos ? team.logos[0].alt : "";
-
     return (
-        <Box sx={mainStandingListItemBoxStyling}>
-            <Paper className="main-paper" elevation={5}>
-                <img src={teamImage} alt={imageAlt} />
-                <Box>
-                    <Box display="flex">
-                        <Typography variant="h6" fontWeight="300" className="team-name">
-                            {team.displayName}
-                        </Typography>
+        <Grow in={true} timeout={{ enter: index * 100 }}>
+            <Box sx={mainStandingListItemBoxStyling}>
+                <Paper className="main-paper" elevation={5}>
+                    <img src={teamImage} alt={imageAlt} />
+                    <Box>
+                        <Box display="flex">
+                            <Typography variant="subtitle1" fontWeight="300" className="team-name">
+                                {team.displayName}
+                            </Typography>
+                            <Typography className="team-name-abbrev" variant="subtitle2">
+                                {team.abbreviation}
+                            </Typography>
+                        </Box>
                     </Box>
-                    <Typography>{team.abbreviation}</Typography>
-                </Box>
-            </Paper>
-        </Box>
+                </Paper>
+            </Box>
+        </Grow>
     );
 };
 export default StandingListItem;
