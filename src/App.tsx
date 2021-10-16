7735b1e27928abecfa3f56c167fb35239ee4c271
fobdev <silvapedro55@gmail.com>
2021-10-16 13:39:13 -0300

    primeira implementação do react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import "./App.css";
-import { Box } from "@mui/system";
 import Home from "./pages/Home";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+// import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
     return (
-        <Box>
-            <Home />
-        </Box>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="*">
+                    <Typography variant="h3" fontWeight="200" textAlign="left" margin="1em">
+                        404 - Not Found
+                    </Typography>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
